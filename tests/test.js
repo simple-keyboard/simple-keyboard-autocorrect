29e5cc279ca1a927f987565df7b0dbe4b8b9fd93
Francisco Hodge <hello@franciscohodge.com>
2019-06-15 00:03:20 -0400

    Tests update

--- tests/test.js
+++ tests/test.js
@@ -7,24 +7,6 @@ test('Runs without crashing', () => {
   div.className = "simple-keyboard";
   document.body.appendChild(div);
 
-  new Keyboard({
-    debug: true,
-    onChange: input => input,
-    onKeyPress: button => button,
-    newLineOnEnter: true,
-    autocorrectDict: ["dog", "house"],
-    modules: [
-      SimpleKeyboardAutocorrect
-    ]
-  });
-});
-
-it('Keyboard disableButtonHold will work', () => {
-  const div = document.createElement('div');
-  
-  div.className = "simple-keyboard";
-  document.body.appendChild(div);
-
   let keyboard = new Keyboard({
     debug: true,
     onChange: input => input,
@@ -36,8 +18,6 @@ it('Keyboard disableButtonHold will work', () => {
       SimpleKeyboardAutocorrect
     ]
   });
-
-  console.log(keyboard.getButtonElement("o"));
 
   keyboard.getButtonElement("d").onclick();
   keyboard.getButtonElement("o").onclick();
