0f5c44dad2fc688a591400faa5316ad04d19e82d
Francisco Hodge <hello@franciscohodge.com>
2019-06-14 22:30:18 -0400

    npm update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard-autocorrect v2.0.3
+ *   simple-keyboard-autocorrect v2.0.4
  *   https://github.com/hodgef/simple-keyboard-autocorrect
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
