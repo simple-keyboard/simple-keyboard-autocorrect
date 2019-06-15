1335fb190785371f64a24536d7860d9c5508c5c1
Francisco Hodge <hello@franciscohodge.com>
2019-06-14 22:28:17 -0400

    Tests update

--- tests/test.js
+++ tests/test.js
@@ -4,7 +4,7 @@ import SimpleKeyboardAutocorrect from '../src/index';
 test('Runs without crashing', () => {
   const div = document.createElement('div');
   
-  div.className += "simple-keyboard";
+  div.className = "simple-keyboard";
   document.body.appendChild(div);
 
   new Keyboard({
@@ -12,9 +12,37 @@ test('Runs without crashing', () => {
     onChange: input => input,
     onKeyPress: button => button,
     newLineOnEnter: true,
-    autocorrectDict: ["data", "test"],
+    autocorrectDict: ["dog", "house"],
     modules: [
       SimpleKeyboardAutocorrect
     ]
   });
+});
+
+it('Keyboard disableButtonHold will work', () => {
+  const div = document.createElement('div');
+  
+  div.className = "simple-keyboard";
+  document.body.appendChild(div);
+
+  let keyboard = new Keyboard({
+    debug: true,
+    onChange: input => input,
+    onKeyPress: button => button,
+    newLineOnEnter: true,
+    useMouseEvents: true,
+    autocorrectDict: ["dog", "house"],
+    modules: [
+      SimpleKeyboardAutocorrect
+    ]
+  });
+
+  console.log(keyboard.getButtonElement("o"));
+
+  keyboard.getButtonElement("d").onclick();
+  keyboard.getButtonElement("o").onclick();
+  keyboard.getButtonElement("{space}").onclick();
+  keyboard.getButtonElement("{bksp}").onclick();
+
+  expect(keyboard.getInput()).toBe("dog");
 });
