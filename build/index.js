0064aea893e92153974abc425a5ddd3ef5e8b027
Francisco Hodge <hello@franciscohodge.com>
2020-02-06 00:07:34 -0500

    Added npmignore

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard-autocorrect v2.0.94
+ *   simple-keyboard-autocorrect v2.1.0
  *   https://github.com/hodgef/simple-keyboard-autocorrect
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
