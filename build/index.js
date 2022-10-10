bbb4befacdc320b89c0bce4f7bcc3329562b0eb0
Francisco Hodge <hello@franciscohodge.com>
2022-10-10 08:05:10 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard-autocorrect v2.2.486
+ *   simple-keyboard-autocorrect v2.2.487
  *   https://github.com/hodgef/simple-keyboard-autocorrect
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
